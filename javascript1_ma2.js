2708353c0097b1214079e0d62b999dce853a4884
Fredrik G. Hovde <fghovde@gmail.com>
2019-02-10 12:41:53 +0100

    Task 4 added. Corrected placement of task 2 and 3. Added call for taskOrigin function (for results)

--- javascript1_ma2.js
+++ javascript1_ma2.js
@@ -15,31 +15,38 @@ var result7 = document.getElementById("result7");
 
 // global variables
 var myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var fruitString = "Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.";
+var fruitChange = {Strawberries: "<b>Bananas</b>",
+                   strawberries: "<b>bananas</b>",
+                   Strawberry: "<b>Banana</b>",
+                   strawberry: "<b>banana</b>"};
 
 // function to show initial results
 function taskOrigin() {
     result2.innerHTML = myArray.join(", ");
     result3.innerHTML = myArray.join(", ");
+    result4.innerHTML = fruitString;
 }
 
 //1. Create a function that displays prototypal inheritance
+
+//2. Create an array of numbers from 1 - 10; slice the 5th number in the array
 cut5thBtn.onclick = function () {
     result2.innerHTML = myArray.slice(4, 5);
 };
-//2. Create an array of numbers from 1 - 10; slice the 5th number in the array
+//3. Delete the last number in the array that you created above.
 cutLastBtn.onclick = function () {
     myArray.splice(-1);
     // myArray.pop();
     result3.innerHTML = myArray.join(", ");
 };
-
-//3. Delete the last number in the array that you created above.
-
-
 //4. Given the following paragraph, create a JavaScript function that changes all mentions of strawberry to banana and strawberries to bananas: 
-Strawberries are a popular part of spring and summer diets throughout America. Mouths water from coast to coast each spring, when small white blossoms start to appear on strawberry bushes. They announce the impending arrival of the ruby red berries that so many people crave. Ripe strawberries taste sweet and have only a slight hint of tartness. They are also one of the healthiest fruits around. There are countless recipes for the luscious red berry, but many people prefer to eat them fresh and unaccompanied.
-
-
+changeFruitBtn.onclick = function () {
+    fruitString = fruitString.replace(/Strawberries|strawberries|Strawberry|strawberry/g, function (bananaReplace) {
+        return fruitChange[bananaReplace]; }
+        );
+    result4.innerHTML = fruitString;
+};
 //5. Create an array and a button. The array should contain 4 football clubs names. When the button is clicked, remove all elements out of the array that we just created. There after add in 4 names of cars inside that array. Log it to the console.
 
 
@@ -48,3 +55,4 @@ Strawberries are a popular part of spring and summer diets throughout America. M
 
 //7. Create a simple function that logs the date.
 
+taskOrigin();
